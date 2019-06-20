3057a7e7ddacde8681e1178f8683ca01f8111931
Your Name <email@example.com>
2019-06-20 10:49:06 +0800

    zengjia

---
+++ src/java/hello.java
@@ -0,0 +1,14 @@
+package java;
+
+import sun.applet.Main;
+
+/**
+ * @author Mr Cheng
+ * @date 2019/6/20 - 10:39
+ */
+public class hello {
+    public static void main(String[] args) {
+        System.out.println("learning git!");
+        System.out.println("learning git again!");
+    }
+}
